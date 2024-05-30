3aa6c8edcf5d1e23266cfb1412f8d7010600eb9e
brian-ambani <brainambani1@gmail.com>
2024-05-30 20:49:48 +0300

    updated the folder structure

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="m-5">
+      <Button>Click me</Button>
+      <h2>Hello from the index page</h2>
+    </div>
+  );
+}
