81f9f70bad860baaffdc0a29325f32fe39e081f2
brian-ambani <brainambani1@gmail.com>
2024-05-31 23:42:14 +0300

    added the auth options

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,7 +1,8 @@
 import { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./db";
-import InstagramProvider from "next-auth/providers/instagram";
+import GitHubProvider from "next-auth/providers/github";
+
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 import { Adapter } from "next-auth/adapters";
@@ -10,9 +11,9 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
 
   providers: [
-    InstagramProvider({
-      clientId: process.env.INSTAGRAM_CLIENT_ID as string,
-      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET as string,
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
