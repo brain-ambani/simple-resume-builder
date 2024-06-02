eeabfa1b2b364a0e4dd39af50509637fbdf7121a
brian-ambani <brainambani1@gmail.com>
2024-06-02 11:02:31 +0300

    implemented the nav and log out functionality

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -1,9 +1,15 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
+import { signOut } from "next-auth/react";
 
 export default function UserNav() {
   return (
@@ -11,11 +17,21 @@ export default function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative w-10 h-10 rounded-sm">
           <Avatar className="h-10 w-10 rounded-sm">
-            <AvatarImage />
+            <AvatarImage src="https://pfojdzzmsmnlrjgqwgmg.supabase.co/storage/v1/object/public/user%20image/avatar.png" />
             <AvatarFallback className="rounded-sm">Brn</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="flex flex-col space-y-1">
+          <p className="text-sm font-medium leading-none">Brn</p>
+          <p className="text-xs leading-none text-muted-foreground">
+            brn@gmail.com
+          </p>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>Sign out</DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
